6c2bdd95b160501e6ecf517d960e805a7a189af4
Angel Valentin <angelgvalentin7@gmail.com>
2022-05-06 10:57:31 -0500

    first commit

---
+++ src/features/user.js
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// ? createSlice allows us to create the reducer very easily and makes it intuitive to split the logic and be able to access it throughout the application.
+
+const userSlice = createSlice({});
